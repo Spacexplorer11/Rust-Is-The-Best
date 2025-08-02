56ebfe206b81cf73526d3cf9e65b75d1fe4e164f
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-02 22:20:55 +0100

    Added simple text and rolling rust logo across the screen!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="@container text-center">
+	<h1 class="text-5xl text-rust">Rust. Is. The. Best.</h1>
+
+	<div class="moving-container animate-[move_5s_linear_infinite] opacity-50">
+		<img
+			src="/images/rust.webp"
+			alt="Rust logo"
+			title="rust logo"
+			class="w-[100px] h-[100px] my-4 animate-[spin_5s_linear_infinite] rust-img"
+		/>
+	</div>
+</div>
